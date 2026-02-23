693272d358222d9e2590eac08c7909eae76c2884
Luis Tasayco <luistasayco3030@gmail.com>
2026-02-23 04:22:27 +0000

    actualizacion - fotter nav - rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 
-  base: "/",
+  base: "/2026-3",
   devToolbar: {
       enabled: false,
   },
