03a2425fd2f67c7b986143a266d3bd02d798b444
LTasaycoQ <luistasayco3030@gmail.com>
2026-02-23 16:22:54 -0500

    actualizacion seo mejoras - canonical url - sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 
   base: "/",
+  site: "https://peruluxuryjourneys.com/",
   devToolbar: {
       enabled: false,
   },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()],
+
 });
