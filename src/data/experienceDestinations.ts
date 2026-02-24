3c3bf6f41fc78e47f28c144fe31af668f779137d
LTasaycoQ <luistasayco3030@gmail.com>
2026-02-24 17:15:05 -0500

    actualizacion - de menu - Nueva Seccion Experiencias

---
+++ src/data/experienceDestinations.ts
@@ -0,0 +1,70 @@
+
+
+export interface expDestinatiosData {
+  fondo: string;
+  experienciaLugar: string;
+  titleSEO: string;
+  title: string;
+  description: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+  image5: string;
+}
+
+export const destinationsExp: expDestinatiosData[] = [
+
+  {
+    fondo: "https://res.cloudinary.com/dlgeap8h0/image/upload/banner-about-us3_w9sapa.webp",
+
+    experienciaLugar: "capilla",
+    title: "Lima Capilla",
+    titleSEO:  "Capilla",
+    description: "Lima, known as “the City of Kings”, is usually the first step in a journey to Peru. It is a city that blends modern neighbourhoods with the colonial-style buildings of the Main Plaza. Barranco has a large collection of museums, iconic bars and restaurants, shops and art studios. Miraflores, the tourist district for excellence, offers beautiful parks along its boardwalk, with stunning views of the Pacific Ocean. You can enjoy lovely sunsets from Larcomar, a shopping mall located in the heart of the district. Lima has plenty of colonial buildings to see. It is a must to walk around the Historical Centre of the city, considered as a World Heritage Site by UNESCO. It really worth to visit the Plaza de Armas where the Presidential Palace and the Archbishop Palace are located, the Aliaga House -the oldest Colonial mansion in Lima-, the Municipal Palace and the Monastery of San Francisco, famous for its baroque architecture and for its catacombs. Considered the Gastronomic Capital of Latin America, Lima has become the culinary hub of several huariques -small and family-run restaurants that keep traditional recipes- and many fancy and renowned restaurants included in The World’s 50 Best Restaurants list.",
+  
+    image1: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima1_qwgyub.webp",
+    image2: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima2_obyqay.webp",
+    image3: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image4: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image5: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    
+  },
+
+  {
+    fondo: "https://res.cloudinary.com/dlgeap8h0/image/upload/banner-about-us3_w9sapa.webp",
+
+    experienciaLugar: "/capilla2",
+    title: "Lima ya no es capilla",
+    titleSEO:  "a ya no es capilla",
+    description: "Lim’s 50 Best Restaurants list.",
+  
+    image1: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image2: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima2_obyqay.webp",
+    image3: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image4: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image5: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    
+  },
+
+
+
+    {
+    fondo: "https://res.cloudinary.com/dlgeap8h0/image/upload/banner-about-us3_w9sapa.webp",
+
+    experienciaLugar: "/luis-tasayco",
+    title: "Luis Angel Tasayco Quispe ya no es capilla",
+    titleSEO:  "a ya no es capilla",
+    description: "Lim’s 50 Best Restaurants list.",
+  
+    image1: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image2: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima2_obyqay.webp",
+    image3: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image4: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    image5: "https://res.cloudinary.com/dlgeap8h0/image/upload/Lima3_oipvsa.webp",
+    
+  },
+
+
+
+];
